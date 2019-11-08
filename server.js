7e114af98ea16f10e31fea021e7bc60daf737e58
Helloiain <359556118+Helloiain@users.noreply.github.com>
2019-11-08 16:13:42 -0600

    serve index.js

--- server.js
+++ server.js
@@ -20,3 +20,14 @@ app.use("/api/v1/profile", require("./routes/profile"));
 app.listen(port, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
 });
+
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
