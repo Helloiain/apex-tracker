50d71fc5b47c005b9e648517befc5d98f5aa0353
Helloiain <359556118+Helloiain@users.noreply.github.com>
2019-11-08 16:10:02 -0600

    add build

---
+++ client/build/precache-manifest.89c47804199944eada6e7ec3c3bd4f8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d1bc3d7e75c9d64ac76756810b4694",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c0e04844e09b2a26f22",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "53ed8bfbfc0460067527",
+    "url": "/static/css/main.3cd9272a.chunk.css"
+  },
+  {
+    "revision": "3c0e04844e09b2a26f22",
+    "url": "/static/js/2.271ca99e.chunk.js"
+  },
+  {
+    "revision": "53ed8bfbfc0460067527",
+    "url": "/static/js/main.cf6a29f3.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "badc059db553d3c4d014ad48534b9567",
+    "url": "/static/media/logo.badc059d.png"
+  },
+  {
+    "revision": "605edd6cb7792eeb51e1242cd4f2a2e1",
+    "url": "/static/media/octane.605edd6c.png"
+  }
+]);
